276f3fcf92fc56330290a64627dd333a8899253e
DarkholmeTenk <shaney.booth@gmail.com>
2018-06-18 07:50:11 +0100

    Times

--- src/main/java/io/darkcraft/dnd/combat/CombatSet.java
+++ src/main/java/io/darkcraft/dnd/combat/CombatSet.java
@@ -2,7 +2,10 @@ package io.darkcraft.dnd.combat;
 
 import java.util.List;
 
+import org.joda.time.DateTime;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.index.Indexed;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -12,6 +15,12 @@ public class CombatSet
     @JsonProperty
     @Id
     public String id;
+    @CreatedDate
+    @JsonProperty
+    public DateTime created;
+    @LastModifiedDate
+    @JsonProperty
+    public DateTime updated;
     @JsonProperty
     @Indexed(unique=true)
     public String name;
--- src/main/java/io/darkcraft/dnd/config/MongoConfig.java
+++ src/main/java/io/darkcraft/dnd/config/MongoConfig.java
@@ -3,6 +3,7 @@ package io.darkcraft.dnd.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.util.StringUtils;
@@ -12,6 +13,7 @@ import com.mongodb.MongoClient;
 import io.darkcraft.dnd.store.CharSheetRepository;
 
 @Configuration
+@EnableMongoAuditing
 @EnableMongoRepositories(basePackageClasses=CharSheetRepository.class)
 public class MongoConfig
 {
